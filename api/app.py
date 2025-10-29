d845bf30388cd6fb444b3e69b916fdd0e6a31005
Shoukat Ali <shoukatalisukkur@gmail.com>
2025-10-29 18:23:37 +0500

    Prepare for Vercel deployment

--- api/app.py
+++ api/app.py
@@ -173,7 +173,8 @@ def search():
 
 # Run
 # Only needed for local development
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
